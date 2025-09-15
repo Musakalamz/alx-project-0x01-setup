8913a8462da08e996b97c7f993e65e86fab590dc
Musakalamz <ogunsolumusa@gmail.com>
2025-09-15 23:04:02 +0100

    Setting up Project-1

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,5 @@
+const PostCard: React.FC = () => {
+  return <div></div>;
+};
+
+export default PostCard;
---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,5 @@
+const index: React.FC = () => {
+  return <div></div>;
+};
+
+export default index;
